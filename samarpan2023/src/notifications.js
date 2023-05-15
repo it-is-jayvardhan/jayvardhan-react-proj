db47b45887647555a9b4366c143bda8cf1bf2680
it-is-jayvardhan <grprashanthreddy@outlook.com>
2023-05-15 18:42:15 +0530

    modified and added soem files

--- samarpan2023/src/notifications.js
+++ samarpan2023/src/notifications.js
@@ -1,6 +1,10 @@
 export default function Notification(){
     return (<>
     	    <div className="w3-card w3-container">
+    <h3>New Notification 15/05/2023</h3>
+    <img className="w3-image w3-responsive" src="https://github.com/AMBITION-GROUP-OF-ORG/cbit-samarpan-2023/blob/main/notifications/noti3.jpeg?raw=true" alt="noti_image"/>
+    </div>
+    	    <div className="w3-card w3-container">
     <h3>New Notification 11:00 AM 12/05/2023</h3>
     <img className="w3-image w3-responsive" src="https://github.com/AMBITION-GROUP-OF-ORG/cbit-samarpan-2023/blob/main/samarpan%2012-05-23%20pics/img1.jpeg?raw=true" alt="noti_image"/>
     </div>
